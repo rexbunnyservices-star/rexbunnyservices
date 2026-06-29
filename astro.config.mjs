26b058e12a465dd4903e35465fe9bc43d720ae6d
Anass <hello@rexbunnyservices.com>
2026-06-30 00:23:16 +0530

    Switch to static mode for Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,11 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://rexbunnyservices.com",
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  site: "https://rexbunnyservices.online",
+  output: "static",
   integrations: [preact(), tailwind(), sitemap(), mdx()],
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
-  vite: {
-    ssr: {
-      noExternal: ["pocketbase"],
-    },
-  },
 });
